92f7863c68a86932d73cff0f528b43ebef9053ea
emre almamis <ealmamis56@gmail.com>
2024-03-30 20:58:47 +0300

    static dosyaları yüklendi

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
-const fn = function () {
-  console.log('function working');
-};
+//request ve response arasındaki herşey middleweardir
+
+const express = require('express');
+const path = require('path')
+const app = express();
+
+
+
+//MİDDLEWEARS
+app.use(express.static('public'))//static dosyalar için public klasörü
+
+
+//http metodlarını kullanbiliriz
+app.get('/',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'temp/index.html'))
+})
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`server ${port} portunda çalışmmaya başladı`);
+});

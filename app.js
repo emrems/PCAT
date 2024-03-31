4fddeb1587f76186e7bfcf5561bda23e0185013e
emre almamis <ealmamis56@gmail.com>
2024-03-31 12:48:50 +0300

    partials oluştruldu

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
 //request ve response arasındaki herşey middleweardir
 
 const express = require('express');
-const path = require('path')
+const path = require('path');
+const ejs = require('ejs');
 const app = express();
 
-
+//TEMPLATE ENGİNE
+app.set('view engine', 'ejs');
 
 //MİDDLEWEARS
-app.use(express.static('public'))//static dosyalar için public klasörü
+app.use(express.static('public')); //static dosyalar için public klasörü
+
+//ROUTES
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
 
 
-//http metodlarını kullanbiliriz
-app.get('/',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'temp/index.html'))
-})
+app.get('/add', (req, res) => {
+  res.render('add');
+});
+
 
 const port = 3000;
 app.listen(port, () => {
